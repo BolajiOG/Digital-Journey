425e16645d0cc67c5206f3642058e699ebe1d251
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2022-04-02 21:15:36 +0100

    Add 2022-04-02 weekend quiz

---
+++ QUIZ.js
@@ -0,0 +1,12 @@
+/**
+ * WEEKEND QUIZ
+ * Create four "product" objects using a constructor function.
+ * Each "product" must have a numeric "id", a string "name" and a float "price".
+ * 
+ * Create four literal "cart_item" objects without a constructor function or Object.create.
+ * Each "cart_item" must have a numeric "quantity", a "product" object and a "subtotal" function that returns "product" "price" values multiplied by "quantity".
+ * All "cart_item" objects must use the same "subtotal" function.
+ *
+ * Create an empty "cart" object using Object.create.
+ * The "cart" must have four "cart_item" objects and a "total" function that returns the sum of all "cart_item" "subtotal" values.
+ */
