12988d0174c56e683c0a29a9195892a2e7db4668
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2022-04-05 18:13:59 +0100

    Fix Quiz issue

--- QUIZ.js
+++ QUIZ.js
@@ -2,8 +2,6 @@ console.log("\n************ WEEKEND QUIZ *************\n\n");
 // Create four "product" objects using a constructor function.
 // Each "product" must have a numeric "id", a string "name" and a float "price".
 
-
-
 function ObjQ (id, name, price) {
         this.id = id;
         this.name = name;
@@ -15,8 +13,6 @@ let prod1 = new ObjQ (1, 'milk', 16.0);
 let prod2 = new ObjQ (2, 'egg', 6.0);
 let prod3 = new ObjQ (3, 'beef', 46.0);
 let prod4 = new ObjQ (4, 'jam', 23.0);
-
-
  
 // Create four literal "cart_item" objects without a constructor function or Object.create.
 // Each "cart_item" must have a numeric "quantity", a "product" object and a "subtotal" function that returns "product" "price" values multiplied by "quantity".
@@ -39,38 +35,45 @@ cart_item1.sub = subtotal;
 let cart_item2 = {};
 cart_item2.prod = prod2;
 cart_item2.quantity = 10;
-cart_item2.sub =  subtotal;
+cart_item2.sub = subtotal;
 
 let cart_item3 = {};
 cart_item3.prod = prod3;
 cart_item3.quantity = 2;
-cart_item3.sub =  subtotal;
+cart_item3.sub = subtotal;
 
 
 let cart_item4 = {};
 cart_item4.prod = prod1;
 cart_item4.quantity = 10;
-cart_item4.sub =  subtotal;
+cart_item4.sub = subtotal;
 
 // Create an empty "cart" object using Object.create.
 // The "cart" must have four "cart_item" properties and a "total" function that returns the sum of all "cart_item" "subtotal" values.
 let cart = Object.create(null);
+// Storing "cart_item" objects as properties.
+cart.item1 = cart_item1
+cart.item2 = cart_item2
+cart.item3 = cart_item3
+cart.item4 = cart_item4
+cart.total = function total () { 
+    return this.item1.sub() + this.item2.sub() + this.item3.sub() + this.item4.sub();
+};
+
+// Storing "cart_item" objects in an array property.
 cart.items = [cart_item1, cart_item2, cart_item3, cart_item4];
-// cart.item1 = cart_item1
-// cart.item2 = cart_item2
-// cart.item3 = cart_item3
-// cart.item4 = cart_item4
-// cart.total = funtion () {
-//     // let total = 0;
-//     // for (let item of this.items){ total += item.sub(); }
-//     // return total;
-// };
 
-// cart.total = function total () { return this.item1.sub() + this.item2.sub() + this.item3.sub() + this.item4.sub(); };
+// Using for loop to calculate total.
+cart.total1 = function () {
+    let total = 0;
+    for (let item of this.items){ total += item.sub(); }
+    return total;
+};
 
-// console.log('The Price is', cart.total())
+// Using a reducer function to calculate total.
+cart.total2 = function() { return this.items.reduce((total, item) => total + item.sub(), 0); }
 
 
-
-
-
+console.log('The Price is', cart.total());
+console.log('The Price is', cart.total1());
+console.log('The Price is', cart.total2());
